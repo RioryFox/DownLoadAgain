965959f071265e934266c7fee971608694a646a1
RioryFox <85555246+RioryFox@users.noreply.github.com>
2024-01-18 18:38:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import vk_api
-
-search_chat = ['Хохочущий Гроб Транзакции']
-
-
-session_api = vk_session19.get_api()
-
-
-
-lol = session_api.messages.getConversations()
-lol = lol['items']
-for ing in lol:
-    ing = ing['conversation']
-    if ing['peer']['type'] == 'chat':
-        if ing['chat_settings']['title']in search_chat:
-            print(ing['peer'], ing['chat_settings']['title'])
+import os
+os.startfile("D:\picture.png")
+print('HAHHAHAHAHAH ANONIMUS!')
